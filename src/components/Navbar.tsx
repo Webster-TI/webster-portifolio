d57a36e16134da73656821373f3a25be8751c0d6
Webster-TI <ti.spacacheri@gmail.com>
2026-01-26 00:28:11 -0300

    fix: navbar routing to app routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,20 @@ export default function Navbar() {
   return (
     <header className="border-b border-neutral-200">
       <div className="mx-auto max-w-6xl px-6 py-4 flex justify-between">
-        <span className="font-semibold">Webster.dev</span>
+        <Link href="/" className="font-semibold">
+          Webster.dev
+        </Link>
 
         <nav className="flex gap-4 text-neutral-700">
-          <Link href="#projects">Projetos</Link>
-          <Link href="#contact">Contato</Link>
+          <Link href="/projects" className="hover:underline">
+            Projetos
+          </Link>
+          <Link href="/about" className="hover:underline">
+            Sobre
+          </Link>
+          <Link href="/contact" className="hover:underline">
+            Contato
+          </Link>
         </nav>
       </div>
     </header>
